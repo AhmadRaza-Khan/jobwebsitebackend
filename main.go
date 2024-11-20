59de8eb52f5964416f1ed9ecb765f5d64bd90d67
AhmadRaza-Khan <ahmadrazakhanbwn.com>
2024-11-20 11:43:01 +0500

    changes

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/ahmadraza-khan/jobwebsite/config"
+	"github.com/ahmadraza-khan/jobwebsite/routes"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	config.ConnectDB()
+}
+
+func Handler() {
+	r := gin.Default()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	routes.Routes(r)
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.File("index.html")
+	})
+	log.Fatal(r.Run(port))
+}
