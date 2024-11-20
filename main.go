cc69c5f6c245e1b90088a29d470aac551ff7d53f
AhmadRaza-Khan <ahmadrazakhanbwn.com>
2024-11-20 11:22:24 +0500

    changes

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
-	"os"
 
 	"github.com/ahmadraza-khan/jobwebsite/config"
 	"github.com/ahmadraza-khan/jobwebsite/routes"
@@ -22,22 +19,14 @@ func init() {
 	config.ConnectDB()
 
 	router = gin.Default()
+
 	routes.Routes(router)
+
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.File("index.html")
 	})
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	router.ServeHTTP(w, r)
-}
-
-func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	fmt.Printf("Starting server on port %s...\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, http.HandlerFunc(Handler)))
+func Handler(c *gin.Context) {
+	router.HandleContext(c)
 }
